44ee6eac7994f426738df8bfa6b865a9c24acda6
Thijs Debets <thijs@tdebets.com>
2019-02-11 22:37:47 +0100

    better Domoticz detection during setup, step 1

--- setup.sh
+++ setup.sh
@@ -11,51 +11,53 @@ mkdir ${InstallDir}/data
 mkdir ${InstallDIr}/log
 mkdir /var/tmp/nwd
 
+echo "Welcome to the NetWorkDetection setup."
+echo "First we have to get arp-scan, the core where this tool is build on top."
+sudo apt-get update
+sudo apt-get install -y arp-scan
 
-echo "Lets set up NWD"
-echo " First we'll install arpscan, please confirm:" 
-sudo apt-get install arp-scan
 
 echo " Detecting Domoticz" 
 if [ "$( sudo service domoticz status | grep -i 'Home Automation System' )" != "" ] ; then 
 	echo "Domoticz detected, using Domoticz (UseDomoticz=YES)" 
 	writesetting "UseDomoticz" "YES"
 
-
-
-
-
-
-
-DomoIP='127.0.0.1'             # Domoticz default IP Address
-DomoPort=$( sudo service domoticz status | grep -i ' -www ' | tr ' ' '\n' | grep -A1 "\-www" | tail -n1 )                # Domoticz Port
-echo "Check Domoticz status"
-DomoticzStatus=$(curl -m 10 -s "http://${DomoIP}:${DomoPort}/json.htm?type=command&param=getSunRiseSet" | grep "\"status\" : \"OK\"")
-while [ "$DomoticzStatus" == "" ] ; do
-    echo "Domoticz not found on ${DomoIP}:${DomoPort}"
-	echo " Please provide ip-address" 
-	read DomoIP
-	echo " Please provide port" 
-	read DomoPort
+	DomoIP='127.0.0.1'             # Domoticz default IP Address
+	DomoPort=$( sudo service domoticz status | grep -i ' -www ' | tr ' ' '\n' | grep -A1 "\-www" | tail -n1 )                # Domoticz Port
+	echo "Check Domoticz status"
 	DomoticzStatus=$(curl -m 10 -s "http://${DomoIP}:${DomoPort}/json.htm?type=command&param=getSunRiseSet" | grep "\"status\" : \"OK\"")
+	while [ "$DomoticzStatus" == "" ] ; do
+		echo "Domoticz not found on ${DomoIP}:${DomoPort}"
+		echo " Please provide ip-address" 
+		read DomoIP
+		echo " Please provide port" 
+		read DomoPort
+		DomoticzStatus=$(curl -m 10 -s "http://${DomoIP}:${DomoPort}/json.htm?type=command&param=getSunRiseSet" | grep "\"status\" : \"OK\"")
 
-done
-writesetting "DomoIP" "'${DomoIP}'" 
-writesetting "DomoPort" "'${DomoPort}'" 
+	done
+	writesetting "DomoIP" "'${DomoIP}'" 
+	writesetting "DomoPort" "'${DomoPort}'" 
 
-echo " Please provide pin" 
-read DomoPin
-writesetting "DomoPIN" "'${DomoPin}'" 
+	echo "Please provide pin" 
+	read DomoPin
+	writesetting "DomoPIN" "'${DomoPin}'" 
 
+	echo "Let's check if there is already a NWD configured" 
+	
+	HardwareIDX=$( curl -m 10 -s "http://${DomoIP}:${DomoPort}/json.htm?type=hardware | grep -A 8 NWDhardware | grep idx | cut -d'"' -f4)
+	if [ "${HardwareIDX}" == "" ] then 
+		echo "No previous install detected, creating a Hardware Type" 
+		HardwareIDX=$( curl -m 10 -s "http://${DomoIP}:${DomoPort}/json.htm?type=command&param=addhardware&htype=15&port=1&name=NWDhardware&enabled=true" | tr ',' '\n' | grep idx | cut -d'"' -f4 )
+	fi
+	writesetting "HardwareIDX" "${HardwareIDX}" 
 
-HardwareIDX=$( curl -m 10 -s "http://${DomoIP}:${DomoPort}/json.htm?type=command&param=addhardware&htype=15&port=1&name=NWDhardware&enabled=true" | tr ',' '\n' | grep idx | cut -d'"' -f4 )
-writesetting "HardwareIDX" "${HardwareIDX}" 
+	NewDevicesFoundIDX=$( curl -m 10 -G "http://${DomoIP}:${DomoPort}/json.htm
+	
+	NewDevicesFoundIDX=$( curl -m 10 -G "http://${DomoIP}:${DomoPort}/json.htm" --data "type=createvirtualsensor" --data "idx=$HardwareIDX" --data-urlencode "sensorname=New Device Detected"  --data "sensortype=6"  | tr ',' '\n' | grep idx | cut -d'"' -f4 )
+	writesetting "NewDevicesFoundIDX" "${NewDevicesFoundIDX}" 
 
-NewDevicesFoundIDX=$( curl -m 10 -G "http://${DomoIP}:${DomoPort}/json.htm" --data "type=createvirtualsensor" --data "idx=$HardwareIDX" --data-urlencode "sensorname=New Device Detected"  --data "sensortype=6"  | tr ',' '\n' | grep idx | cut -d'"' -f4 )
-writesetting "NewDevicesFoundIDX" "${NewDevicesFoundIDX}" 
-
-NWDScriptRunning=$( curl -m 10 -G "http://${DomoIP}:${DomoPort}/json.htm" --data "type=createvirtualsensor" --data "idx=$HardwareIDX" --data-urlencode "sensorname=NWDScriptRunning"  --data "sensortype=6"  | tr ',' '\n' | grep idx | cut -d'"' -f4 )
-writesetting "NWDScriptRunning" "${NWDScriptRunning}" 
+	NWDScriptRunning=$( curl -m 10 -G "http://${DomoIP}:${DomoPort}/json.htm" --data "type=createvirtualsensor" --data "idx=$HardwareIDX" --data-urlencode "sensorname=NWDScriptRunning"  --data "sensortype=6"  | tr ',' '\n' | grep idx | cut -d'"' -f4 )
+	writesetting "NWDScriptRunning" "${NWDScriptRunning}" 
 
 else
 	echo "Domoticz NOT detected (UseDomoticz=NO)" 
